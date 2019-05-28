ea2dd25563270bab4803dc891d629d1ae0bed2dc
747624 <747624@123-PC31.student.Tor.Loc>
2019-05-28 15:29:35 -0400

    ex345

--- src/weekfour/main.java
+++ src/weekfour/main.java
@@ -1,5 +1,7 @@
 package weekfour;
 
+import javax.print.DocFlavor.STRING;
+
 public class main {
 
 	public static void main(String[] args) {
@@ -7,8 +9,24 @@ public class main {
 		String name ="harman";
        System.out.println(name);
        int numChar =name.length();
-       System.out.println("number of characters" +numChar);
+       System.out.println("number of characters  "+numChar);
        
+       char charAtps=name.charAt(4);
+       System.out.println("ps of character 4  "+charAtps);
+       
+       
+       String sub =name.substring(0, 2);
+       System.out.println("sub string  "+sub);
+       
+       String a ="gundeep";
+       String b ="gunde";
+       
+       if(a.equals(b)){
+    	   System.out.println("a &b are same ");  
+       }
+       else{
+    	   System.out.println("a &b are not same ");  
+       }
        
        
        
