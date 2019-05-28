d005f4e0649791cbe6317777cfa267a67fb6e195
747624 <747624@123-PC31.student.Tor.Loc>
2019-05-28 15:18:25 -0400

    ex12

---
+++ src/weekfour/main.java
@@ -0,0 +1,19 @@
+package weekfour;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name ="harman";
+       System.out.println(name);
+       int numChar =name.length();
+       System.out.println("number of characters" +numChar);
+       
+       
+       
+       
+       
+       
+	}
+
+}
