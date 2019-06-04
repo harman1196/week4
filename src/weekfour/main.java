e3dc248d00c9ae70a529a6d3cb3ff09db5b555ee
747624 <747624@123-PC25.student.Tor.Loc>
2019-06-04 14:16:54 -0400

    tax

--- src/weekfour/main.java
+++ src/weekfour/main.java
@@ -6,39 +6,11 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name ="harman";
-       System.out.println(name);
-       int numChar =name.length();
-       System.out.println("number of characters  "+numChar);
-       
-       char charAtps=name.charAt(4);
-       System.out.println("ps of character 4  "+charAtps);
-       
-       
-       String sub =name.substring(0, 2);
-       System.out.println("sub string  "+sub);
-       
-       String a ="gundeep";
-       String b ="gunde";
-       
-       if(a.equals(b)){
-    	   System.out.println("a &b are same ");  
-       }
-       else{
-    	   System.out.println("a &b are not same ");  
-       }
-       
-       String j ="hugo";
-       
-    		   System.out.println(j.toUpperCase());  
-      
-    		   String k ="SDFG";
-    	       
-    		   System.out.println(k.toLowerCase());  
-       	   
-    		   
-    		   
-    		   
+		
+    	public static void calcTax(double price){	   
+    	final double TAXRATE =13/100;	   
+    	double result = price*TAXRATE;
+    	System.out.println("tax is :" +result);
 	}
 
 }
